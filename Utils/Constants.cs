66dff9911df87eeb3d70927c7426e48ca00a90d1
RX14 <chris@rx14.co.uk>
2014-03-08 07:23:53 +0000

    Add some Minecraft-Related utils

---
+++ Utils/Constants.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RX14.Utils
+{
+    class Constants
+    {
+        /// <summary>
+        /// https://minotar.net/helm/
+        /// Base download url for downloading Helmets
+        /// </summary>
+        public const string HelmDownloadURL = "https://minotar.net/helm/";
+
+        /// <summary>
+        /// https://s3.amazonaws.com/Minecraft.Download/
+        /// Base MinecraftDownload URL
+        /// </summary>
+        public const string MinecraftDownload = "https://s3.amazonaws.com/Minecraft.Download/";
+
+        /// <summary>
+        /// https://s3.amazonaws.com/Minecraft.Download/versions/
+        /// Base Minecraft Versions API URL
+        /// </summary>
+        public const string MinecraftVersionsBase = MinecraftDownload + "versions/";
+    }
+}
---
+++ Utils/Minecraft/Skin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Net;
+using System.IO;
+using RX14.Utils;
+
+
+namespace RX14.Utils.Minecraft
+{
+    /// <summary>
+    /// Class for handling skins
+    /// </summary>
+    public class Skin
+    {
+        /// <summary>
+        /// Gets a BitmapImage of a head from a Minecraft Username
+        /// </summary>
+        /// <param name="username">The Minecraft Username</param>
+        /// <param name="imgSize">The side length of the image to return.</param>
+        /// <returns>Returns a BitmapImage</returns>
+        public static BitmapImage getUserHead(string username, int imgSize = -1, bool silent = false, string[] errorAction = null)
+        {
+            if (!silent) Logging.logMessage("Trying to display head for " + username + "...");
+
+            string minotarLocation;
+
+            //Change download URL depending on whether imgSize is specified
+            if (imgSize == -1)
+            {
+                minotarLocation = Constants.HelmDownloadURL + username + ".png";
+            } else {
+                minotarLocation = Constants.HelmDownloadURL + username + "/" + imgSize + ".png";
+            }
+
+            HTTP.downloadFile(minotarLocation, "cache/UserHeads/" + username, silent: true);
+
+            return Image.loadBitmapFromFile("cache/UserHeads/" + username + "/" + minotarLocation.Split('/')[minotarLocation.Split('/').Length - 1], UriKind.Relative);
+        }
+    }
+}
