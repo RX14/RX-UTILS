a34122eb6bc0abf130bd732622bc6722d8fe400f
RX14 <chris@rx14.co.uk>
2014-03-12 22:15:08 +0000

    Add sha1sumString method + Rename sha1sum to sha1sumFile and add Obsolete sha1sum method

--- Utils/Sum.cs
+++ Utils/Sum.cs
@@ -18,7 +18,7 @@ namespace RX14.Utils
         /// </summary>
         /// <param name="filelocation">Filename to sum</param>
         /// <returns>The SHA1 in string format</returns>
-        public static string sha1sum(string filelocation)
+        public static string sha1sumFile(string filelocation)
         {
             FileStream fs = new FileStream(filelocation, FileMode.Open);
             BufferedStream bs = new BufferedStream(fs);
@@ -34,6 +34,20 @@ namespace RX14.Utils
             fs.Close();
 
             return sha1.ToString();
+        }
+
+        [Obsolete("Use sha1sumFile method")]
+        public static string sha1sum(string filelocation)
+        {
+            return sha1sumFile(filelocation);
+        }
+
+        public static string sha1sumString(string str)
+        {
+            using (SHA1Managed _sha1 = new SHA1Managed())
+            {
+                return BitConverter.ToString(_sha1.ComputeHash(Encoding.UTF8.GetBytes(str))).Replace("-", string.Empty);
+            }
         }
     }
 }
